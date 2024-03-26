657ed98c324d1936382377f31bd39eda1817ea55
Kotovar <neasit3@gmail.com>
2024-03-27 00:55:58 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import mapGenerator from './utils/mapGenerator';
+
+function App() {
+	mapGenerator();
+
+	return <h1>Генератор карт</h1>;
+}
+
+export default App;
---
+++ src/utils/mapGenerator.ts
@@ -0,0 +1,3 @@
+export default function mapGenerator() {
+	console.log('test');
+}
